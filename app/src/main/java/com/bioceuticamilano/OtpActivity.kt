137e36bd818abf345697625a83b106070f348288
Talhaasif92 <40741974+Talhaasif92@users.noreply.github.com>
2025-09-24 21:26:56 +0500

    Add OTP Screen

---
+++ app/src/main/java/com/bioceuticamilano/OtpActivity.kt
@@ -0,0 +1,11 @@
+package com.bioceuticamilano
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OtpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
